ed483fae68e3920fdccfdbf88acda7ffc4a3516e
Mahadi Hasan <mahadimhs787@gmail.com>
2025-08-05 23:33:03 +0600

    global error added

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 
 import cors from 'cors'
 import { router } from './app/routes';
+import { globalErrorHandler } from './app/middlewares/globarErrorHandler';
 
 export const app = express()
 
@@ -14,3 +15,5 @@ app.use('/api/v1', router)
 app.get('/', (req :Request, res : Response) => {
     res.send({message : "PH tour management system is running...."})
 })
+
+app.use(globalErrorHandler)
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,16 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import httpstatus from 'http-status-codes';
 import { userService } from "./user.service";
 
-const createUser = async (req: Request, res: Response) => {
+const createUser = async (req: Request, res: Response, next : NextFunction) => {
     try {
         const user = await userService.createUser(req.body)
 
         res.status(httpstatus.CREATED).json({ message: 'User created successfully', user })
 
     } catch (error: any) {
-        res.status(httpstatus.BAD_REQUEST).json({ message: `Something went wrong ${error.message}`, error })
+        next(error)
     }
 }
 
